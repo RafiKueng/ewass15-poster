f79c19bc0566fe334e2b2d596597cf473f7ec892
Rafael Kueng <rafik@physik.uzh.ch>
2015-06-16 16:54:01 +0200

    updated arrangement

--- qr/create.py
+++ qr/create.py
@@ -7,10 +7,12 @@ factory = qrcode.image.svg.SvgPathImage
 
 codes = {
     "coll_model_letter": "http://letters.zooniverse.org/letters/86-collaborative_gravitational_lens_modelling_using_spaghettilens_a_spacewarps_project",
+    "letter_simple": "http://letters.zooniverse.org",
     "spaghetti" : "http://labs.spacewarps.org/spaghetti/",
     'arxiv_marshall' : 'http://arxiv.org/abs/1504.06148',
     'arxiv_more'     : 'http://arxiv.org/abs/1504.05587',
-    'arxiv_kueng'    : 'http://arxiv.org/abs/1502.00008'
+    'arxiv_kueng'    : 'http://arxiv.org/abs/1502.00008',
+    'sw_talk'        : 'http://talk.spacewarps.org/#/boards/BSW0000006'
 }
 
 
